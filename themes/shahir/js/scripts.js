37ed1dd89326f090294fcc1ea9c984dcbda8213e
Masoud Gharagozlu <gharagozlu.masoud@gmail.com>
2016-04-04 14:33:37 +0430

    Implementation theme

--- themes/shahir/js/scripts.js
+++ themes/shahir/js/scripts.js
@@ -1,37 +1,12 @@
 $(function() {
 
-    $('body').on('click', '.add-multipliable-input', function(){
-        var input=document.createElement('input');
-        input.type='text';
-        input.name='Apps[permissions]['+$('.multipliable-input').length+']';
-        input.placeholder='دسترسی';
-        input.className='form-control multipliable-input';
-        var container=document.getElementsByClassName('multipliable-input-container');
-        $(container).append(input);
-        return false;
-    });
-
-    $('body').on('click', '.remove-multipliable-input', function(){
-        if($('.multipliable-input').length>1)
-            $('.multipliable-input-container .multipliable-input:last').remove();
-        return false;
-    });
-
-    if($(".app-description").height()>230)
-        $(".app-description").next().show();
-    $('body').on('click', '.more-text', function(){
-        var $h = $(".app-description").height();
-        if($(this).parent().hasClass('open'))
-        {
-            $(this).parent().animate({height:230},0).removeClass('open');
-            $(this).find('span').html('توضیحات بیشتر');
-        }
-        else if($h>230)
-        {
-            $(this).parent().animate({height:$h+80},0).addClass('open');
-            $(this).find('span').html('بستن');
-        }
-        return false;
+    $("html").niceScroll({
+        railalign:'left',
+        rtlmode:true,
+        hwacceleration:false,
+        zindex:1000,
+        cursorcolor:'rgba(0,0,0,0.4)',
+        cursorborder:'none'
     });
 
     $("body").on("click" ,"[href^='#']",function(e) {
