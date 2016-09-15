79cd5ba66c287fdbbf1fe56969d212b7cb69901f
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2016-09-15 11:50:22 +0430

    Fix Bugs

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -311,88 +311,93 @@ if($classes) :
         <?
         endforeach;
         Yii::app()->clientScript->registerScript("owl-carousel-class-script","
-                var owlClasses =  $('.classes-carousel');
-                owlClasses.owlCarousel({
-                    ".(Yii::app()->language == 'fa'?'rtl:true,':'')."
-                    navText:['<span class=\"arrow\"></span>','<span class=\"arrow\"></span>'],
-                    navClass: ['owl-prev disabled','owl-next'],
-                    callbacks: true,
-                    info: true,
-                    margin:30,
-                    responsive : {
-                        0 : {
-                            items:1,
-                            nav : false,
-                            margin :15,
-                            dots : true,
-                            stagePadding : 0
+                var options = {
+                        ".(Yii::app()->language == 'fa'?'rtl:true,':'')."
+                        navText:['<span class=\"arrow\"></span>','<span class=\"arrow\"></span>'],
+                        navClass: ['owl-prev disabled','owl-next'],
+                        callbacks: true,
+                        info: true,
+                        margin:30,
+                        responsive : {
+                            0 : {
+                                items:1,
+                                nav : false,
+                                margin :15,
+                                dots : true,
+                                stagePadding : 0
+                            },
+                            459 :{
+                                items:1,
+                                nav : true,
+                                margin :50,
+                                dots : false,
+                                stagePadding : 50
+                            },
+                            768 :{
+                                items:1,
+                                nav : true,
+                                margin :10,
+                                dots : false,
+                                stagePadding : 0
+                            },
+                            1025 :{
+                                items:2,
+                                nav : true,
+                                margin :40,
+                                dots : false,
+                                stagePadding : 0
+                            },
+                            1201 :{
+                                items:3,
+                                nav : true,
+                                margin :10,
+                                dots : false,
+                                stagePadding : 0
+                            },
+                            1401 :{
+                                items:3,
+                                nav : true,
+                                margin :0,
+                                dots : false,
+                                stagePadding : 0
+                            },
+                            1601 :{
+                                items:4,
+                                nav : true,
+                                margin :0,
+                                dots : false,
+                                stagePadding : 0
+                            },
                         },
-                        459 :{
-                            items:1,
-                            nav : true,
-                            margin :50,
-                            dots : false,
-                            stagePadding : 50
-                        },
-                        768 :{
-                            items:1,
-                            nav : true,
-                            margin :10,
-                            dots : false,
-                            stagePadding : 0
-                        },
-                        1025 :{
-                            items:2,
-                            nav : true,
-                            margin :40,
-                            dots : false,
-                            stagePadding : 0
-                        },
-                        1201 :{
-                            items:3,
-                            nav : true,
-                            margin :10,
-                            dots : false,
-                            stagePadding : 0
-                        },
-                        1401 :{
-                            items:3,
-                            nav : true,
-                            margin :0,
-                            dots : false,
-                            stagePadding : 0
-                        },
-                        1601 :{
-                            items:4,
-                            nav : true,
-                            margin :0,
-                            dots : false,
-                            stagePadding : 0
-                        },
-                    },
-                    onTranslated: $(this).on('translated.owl.carousel', function(e) {
-                        var items_per_page = e.page.size;
-                        var nav_container = $('.classes-carousel .owl-nav');
-                        var item_index = e.item.index;
-                        var item_count = e.item.count;
-                        var last_vis_item_index = items_per_page + item_index;
-                        //$('.classes-carousel').find('.active').not('').removeClass
-                        if(last_vis_item_index === item_count){
-                            $(nav_container).find('div:last').addClass('disabled');
-                        }
-                        else{
-                            $(nav_container).find('div:last').removeClass('disabled');
-                        }
-                        if(item_index != 0){
-                            $(nav_container).find('div:first').removeClass('disabled');
-                        }
-                        else{
-                            $(nav_container).find('div:first').addClass('disabled');
-                        }
-                    }),
-                });
-                $('.classes .nav a').on('shown.bs.tab', function(event){
-                    owlClasses.trigger('refresh.owl.carousel');
+                        onTranslated: $(this).on('translated.owl.carousel', function(e) {
+                            var items_per_page = e.page.size;
+                            var nav_container = $('.classes-carousel .owl-nav');
+                            var item_index = e.item.index;
+                            var item_count = e.item.count;
+                            var last_vis_item_index = items_per_page + item_index;
+                            //$('.classes-carousel').find('.active').not('').removeClass
+                            if(last_vis_item_index === item_count){
+                                $(nav_container).find('div:last').addClass('disabled');
+                            }
+                            else{
+                                $(nav_container).find('div:last').removeClass('disabled');
+                            }
+                            if(item_index != 0){
+                                $(nav_container).find('div:first').removeClass('disabled');
+                            }
+                            else{
+                                $(nav_container).find('div:first').addClass('disabled');
+                            }
+                        }),
+                    };
+//                $('.classes .nav li:first-of-type a').tab('show');
+                $('.classes .nav a').on('shown.bs.tab',function(){
+                    var thisTag = $(this);
+                    var thisTabId = thisTag.data('target');
+                    var owlClasses = $(thisTabId).find('.classes-carousel');
+                    owlClasses.trigger('destroy.owl.carousel');
+                    owlClasses.html(owlClasses.find('.owl-stage-outer').html()).removeClass('owl-loaded');
+                    owlClasses.owlCarousel(options);
                 });
                 ");
         ?>
