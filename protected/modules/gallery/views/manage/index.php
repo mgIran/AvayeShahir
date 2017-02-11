cca9b0f49789e54caed0b6415117c6bcba6e75de
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2017-02-11 19:35:31 +0330

    Gallery fix bugs and change style

--- protected/modules/gallery/views/manage/index.php
+++ protected/modules/gallery/views/manage/index.php
@@ -13,47 +13,62 @@
         <div class="gallery-carousel">
             <?
             if($models) :
-                Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl.'/js/owl.carousel.min.js');
-                Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl.'/css/animate.min.css');
-                Yii::app()->clientScript->registerScript("owl-carousel-script","
-                $(window).load(function(){
-                $('.gallery-carousel').owlCarousel({
-                    ".(Yii::app()->language == 'fa'?'rtl:true,':'')."
-                    navText:['<span class=\"arrow\"></span>','<span class=\"arrow\"></span>'],
-                    nav:true,
-                    autoHeight : true,
-                    animateIn: 'fadeIn',
-                    animateOut: 'fadeOut',
-                    items:1,
-                    responsive:{
-                        0:{
-                           nav:false,
-                            margin:0,
-                            stagePadding:0,
-                        },
-                        768:{
-                            nav:true,
-                            margin:50,
-                            stagePadding:50,
-                        }
+                Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl.'/css/ekko-lightbox.css');
+                Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl.'/js/ekko-lightbox.min.js',CClientScript::POS_END);
+                Yii::app()->clientScript->registerScript("lightbox-script","
+                    $('body').on('click', '[data-toggle=\"lightbox\"]', function(event) {
+                        event.preventDefault();
+                        $(this).ekkoLightbox();
+                    });
+                ",CClientScript::POS_LOAD);
+
+                Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl.'/js/isotope.pkgd.min.js',CClientScript::POS_END);
+                Yii::app()->clientScript->registerCss('sa','
+                    .gallery-item{
+                        width: calc( 25% - 5px);
+                        height: auto;
+                        overflow: hidden;
+                        display: inline-block;
+                        margin-bottom:5px;
                     }
-                });
-                });");
+                    .gallery-item img{
+                        width: 100%;
+                        margin: 0 auto;
+                        height: auto;
+                    }
+                    .modal-backdrop{
+                        background-color: rgba(0,0,0,1);
+                    }
+                    .modal-backdrop.in{
+                        opacity: 0.8;
+                    }
+                ');
+                Yii::app()->clientScript->registerScript("owl-carousel-script","
+                    $('.gallery-carousel').isotope({
+                        itemSelector : '.gallery-item',
+                        layoutMode: 'masonry',
+                        masonry: {
+                          gutter: 5
+                        }
+                    });
+                ",CClientScript::POS_READY);
                 foreach($models as $model):
                     ?>
                     <div class="gallery-item">
-                        <div class="gallery-pic" data-toggle="tooltip" data-placement="top" title="<?= $model->title ?>">
-                            <img src="<?= Yii::app()->baseUrl.'/uploads/gallery/'.$model->file_name; ?>" alt="<?= $model->title ?>">
-                        </div>
-                        <div class="gallery-detail container-fluid">
-                            <div class="blur-mask" id="blur-mask-<?= $model->id ?>"></div>
-                            <div class="gallery-detail-inner">
-                                <h2><?= $model->title ?></h2>
-                                <p class="text">
-                                    <?= strip_tags($model->desc) ?>
-                                </p>
+                        <a href="<?= Yii::app()->baseUrl.'/uploads/gallery/'.$model->file_name; ?>"
+                           data-toggle="lightbox" data-gallery="gallery"
+
+                        >
+                            <div class="gallery-pic">
+                                <img src="<?= Yii::app()->baseUrl.'/uploads/gallery/'.$model->file_name; ?>" alt="<?= $model->title ?>" style="width: 100%">
                             </div>
-                        </div>
+                            <div class="gallery-detail container-fluid">
+                                <div class="blur-mask" id="blur-mask-<?= $model->id ?>"></div>
+                                <div class="gallery-detail-inner">
+                                    <h2><?= $model->title ?></h2>
+                                </div>
+                            </div>
+                        </a>
                     </div>
                     <?
                     Yii::app()->clientScript->registerCss('blur-mask-'.$model->id,'
