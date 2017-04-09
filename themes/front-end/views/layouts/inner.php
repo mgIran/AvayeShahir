d5df95505e56ea4e945df7dbfec6c4ed01271f4a
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2017-04-09 09:47:11 +0430

    add Multiple Teachers define ability to classes

--- themes/front-end/views/layouts/inner.php
+++ themes/front-end/views/layouts/inner.php
@@ -32,20 +32,20 @@
     $cs->registerCssFile($baseUrl.'/css/ripples.min.css');
     $cs->registerCssFile($baseUrl.'/css/owl.carousel.css');
     $cs->registerCssFile($baseUrl.'/css/font-awesome.css');
-    $cs->registerCssFile($baseUrl.'/css/svg.css?7.3');
-    $cs->registerCssFile($baseUrl.'/css/bootstrap-theme.css?7.3');
-    $cs->registerCssFile($baseUrl.'/css/responsive-theme.css?7.3');
+    $cs->registerCssFile($baseUrl.'/css/svg.css?7.4');
+    $cs->registerCssFile($baseUrl.'/css/bootstrap-theme.css?7.4');
+    $cs->registerCssFile($baseUrl.'/css/responsive-theme.css?7.4');
     if (Yii::app()->params['default_language'] !== Yii::app()->language)
     {
         // @todo add css for multi language
-        $cs->registerCssFile($baseUrl.'/css/bootstrap-theme-'.Yii::app()->language.'.css?7.3');
+        $cs->registerCssFile($baseUrl.'/css/bootstrap-theme-'.Yii::app()->language.'.css?7.4');
     }
 
     $cs->registerScriptFile($baseUrl.'/js/bootstrap.min.js');
     $cs->registerScriptFile($baseUrl.'/js/material.min.js');
     $cs->registerScriptFile($baseUrl.'/js/ripples.min.js');
     $cs->registerScriptFile($baseUrl.'/js/jquery.nicescroll.min.js');
-    $cs->registerScriptFile($baseUrl.'/js/scripts.js?7.3');
+    $cs->registerScriptFile($baseUrl.'/js/scripts.js?7.4');
     ?>
 </head>
 <body id="top" class="inner-page">
