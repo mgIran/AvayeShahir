90903a21e38f0fb97ac7b2b5f3bef482bc027fed
Yusef <yusef.mobasheri@gmail.com>
2018-03-05 16:32:20 +0330

    Google Analytics

--- themes/front-end/views/layouts/_innerFooter.php
+++ themes/front-end/views/layouts/_innerFooter.php
@@ -67,84 +67,14 @@
         </div>
     </div>
 </footer>
-<!---->
-<!--<div id="signup-modal" class="modal fade" role="dialog">-->
-<!--    <div class="modal-dialog">-->
-<!--        --><?//= $this->renderPartial("//layouts/_loading");?>
-<!--        <div class="modal-content">-->
-<!--            <div class="modal-header">-->
-<!--                <button type="button" class="close btn" data-dismiss="modal">-->
-<!--                    &times;</button>-->
-<!--            </div>-->
-<!--            <div class="modal-body">-->
-<!--                <div class="center-block box">-->
-<!--                    <h3>--><?//= Yii::t('app','Sign Up Account') ?><!--</h3>-->
-<!---->
-<!--                    --><?php
-//                    $signUpModel = new Users('agreeTerms');
-//                    $form=$this->beginWidget('CActiveForm', array(
-//                        'id'=>'register-form',
-//                        'enableAjaxValidation'=>false,
-//                        'enableClientValidation'=>true,
-//                        'clientOptions'=>array(
-//                            'validateOnSubmit'=>true,
-//                            'afterValidate' => 'js:function(form ,data ,hasError){
-//                                if(!hasError)
-//                                {
-//                                    var form = $("#register-form");
-//                                    var loading = $(".signup .loading-container");
-//                                    var url = \''.Yii::app()->createUrl('/register').'\';
-//                                    submitAjaxForm(form ,url ,loading ,"if(html.state == \'ok\') location.reload();");
-//                                }
-//                            }'
-//                        ),
-//                        'htmlOptions' => array(
-//                            'class' => 'form-group'
-//                        )
-//                    ));
-//                    Yii::app()->clientScript->registerScript('registerForm','
-//                        document.getElementById("register-form").reset();
-//                    ');
-//                    echo CHtml::hiddenField('ajax','register-form');
-//                    ?>
-<!--                        --><?//= $this->renderPartial("//layouts/_flashMessage");?>
-<!--                        <div class="relative">-->
-<!--                            --><?php //echo $form->emailField($signUpModel,'email' ,array(
-//                                'placeholder' => Yii::t('app','Email'),
-//                                'class' => 'text-field'
-//                            ));
-//                            echo $form->error($signUpModel,'email',array('class'=>'errorMessage tip'));?>
-<!--                        </div>-->
-<!--                        <div class="relative">-->
-<!--                            --><?php //echo $form->passwordField($signUpModel,'password',array(
-//                                'placeholder' => Yii::t('app','Password'),
-//                                'class' => 'text-field'
-//                            ));
-//                            echo $form->error($signUpModel,'password',array('class'=>'errorMessage tip'));?>
-<!--                        </div>-->
-<!--                        <div class="relative">-->
-<!--                            --><?php //echo $form->telField($signUpModel,'phone',array(
-//                                'placeholder' => Yii::t('app','Phone'),
-//                                'class' => 'text-field',
-//                                'max-length' => 11
-//                            ));
-//                            echo $form->error($signUpModel,'phone',array('class'=>'errorMessage tip'));?>
-<!--                        </div>-->
-<!--                        <div class="relative">-->
-<!--                            <div class="checkbox">-->
-<!--                                <label>-->
-<!--                                    --><?//= $form->checkBox($signUpModel,'agreeTerms'); ?>
-<!--                                    <span>-->
-<!--                                --><?//= Yii::t('app' ,'I agree with the <a data-toggle="modal" data-target="#terms-modal" href="#">Terms and Policies</a>')?>
-<!--                            </span>-->
-<!--                                </label>-->
-<!--                                --><?// echo $form->error($signUpModel,'agreeTerms',array('class'=>'errorMessage tip'));?>
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        --><?//= CHtml::submitButton(Yii::t('app','Sign Up'),array('class'=>"button-field btn")); ?>
-<!--                    --><?php //$this->endWidget(); ?>
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115126013-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-115126013-1');
+</script>
