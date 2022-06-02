24c7cd1cb359a1f8c7bc9543e748f1cb3612459b
yusef mobasheri <yusef.mobasheri.business@gmail.com>
2022-06-02 23:27:29 +0430

    Update Enamad

--- protected/config/main.php
+++ protected/config/main.php
@@ -272,7 +272,7 @@ return array(
 		),
 		'default_language' => 'fa',
 		'mailTheme'=>
-				'<div style="display: block;width: 100%;"><h2 style="margin-bottom:0;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;display: block;width: 100%;background-color: #0b3762;line-height:60px;color:#fff;font-size: 24px;text-align: right;padding-right: 50px">پردیس آوای شهیر<span style="font-size: 14px;color:#f0f0f0"> - موسسه فرهنگی هنری آموزش زبان های خارجی</span></h2></div>
+				'<div style="display: block;width: 100%;"><h2 style="margin-bottom:0;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;display: block;width: 100%;background-color: #0b3762;line-height:60px;color:#fff;font-size: 24px;text-align: right;padding-right: 50px">آوای شهیر<span style="font-size: 14px;color:#f0f0f0"> - موسسه فرهنگی هنری آموزش زبان های خارجی</span></h2></div>
              <div style="display: inline-block;width: 100%;font-family:tahoma;line-height: 28px;">
                 <div style="direction:rtl;display:block;overflow:hidden;border:1px solid #efefef;text-align: center;padding:15px;">{MessageBody}</div>
              </div>',
